d2eb4f4505119a60ae05f0171f2ac9b349071df7
ZaleXypher <89084014+ZaleXypher@users.noreply.github.com>
2025-07-18 16:30:21 +0700

    basic project array creation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./style.css";
+import {Project} from "./todo.js";
+
+let test = new Project("Testing")
+test.addToDo("test", "testing", "date", "Low")
+test.addToDo("test2", "testing2", "date2", "Low")
+console.log(test.list)
---
+++ src/todo.js
@@ -0,0 +1,19 @@
+export class Project{
+    constructor (title){
+        this.projectTitle = title;
+        this.todo = [];
+    }
+
+    addToDo(title, description, due = new Date, priority){
+        let temp = {};
+        temp.taskTitle = title;
+        temp.taskDescription = description;
+        temp.due = due;
+        temp.priority = priority;
+        this.todo.push(temp);
+    }
+
+    get list() {
+        return this.todo;
+    }
+}
